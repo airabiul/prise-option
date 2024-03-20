cd977aa4fac5ea4fdb66d7bb28dae38e38f73084
airabiul <rabiulislam074@gmail.com>
2024-03-20 11:27:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-4xl bg-slate-500'>Vite + React</h1>
+
+      <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+
+<div className="card lg:card-side bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
+  <div className="card-body">
+    <h2 className="card-title">New album is released!</h2>
+    <p>Click the button to listen on Spotiwhy app.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Listen</button>
+    </div>
+  </div>
+</div>
+    </>
+  )
+}
+
+export default App
